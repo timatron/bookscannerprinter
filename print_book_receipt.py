3e30751fc75a4b6d666831c36fad3980b17ab5f6
Tim Schwartz <timatron@gmail.com>
2013-03-14 18:08:30 -0700

    working on sizing

--- print_book_receipt.py
+++ print_book_receipt.py
@@ -9,21 +9,21 @@ from lib.pythermalprinter.printer import *
 qr_image = MakeQRImage("http://www.timschwartz.org")
 qr_image.show()
 
-#if len(sys.argv) == 2:
-#     serialport = sys.argv[1]
-#else:
-#    serialport = ThermalPrinter.SERIALPORT
-#
-#if not os.path.exists(serialport):
-#    sys.exit("ERROR: Serial port not found at: %s" % serialport)
+if len(sys.argv) == 2:
+     serialport = sys.argv[1]
+else:
+    serialport = ThermalPrinter.SERIALPORT
+
+if not os.path.exists(serialport):
+    sys.exit("ERROR: Serial port not found at: %s" % serialport)
 
 print "Testing printer on port %s" % serialport
 p = ThermalPrinter(serialport=serialport)
 
-#
-#data = list(qr_image.getdata())
-#w, h = qr_image.size
-#p.print_bitmap(data, w, h, True)
-#p.linefeed()
-#p.linefeed()
-#p.linefeed()
+
+data = list(qr_image.getdata())
+w, h = qr_image.size
+p.print_bitmap(data, w, h, True)
+p.linefeed()
+p.linefeed()
+p.linefeed()
