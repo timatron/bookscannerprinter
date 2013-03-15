c24c75dce1e9a6976c9dc0305bce058a83791395
Tim Schwartz <timatron@gmail.com>
2013-03-14 18:24:00 -0700

    renamed a bit

---
+++ printQrCode.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#coding=utf-8
+import sys, os
+import Image, ImageDraw
+
+from lib.pyqrcode.pyqrcode import *
+from lib.pythermalprinter.printer import *
+
+
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+else:
+    url = "http://www.timschwartz.org"
+
+qr_image = MakeQRImage("http://www.timschwartz.org")
+qr_image.show()
+
+serialport = ThermalPrinter.SERIALPORT
+if not os.path.exists(serialport):
+    sys.exit("ERROR: Serial port not found at: %s" % serialport)
+
+p = ThermalPrinter(serialport=serialport)
+
+data = list(qr_image.getdata())
+w, h = qr_image.size
+p.print_bitmap(data, w, h, True)
+p.linefeed()
+p.linefeed()
+p.linefeed()
