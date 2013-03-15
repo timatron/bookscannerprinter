2cf54dce84b8b08d90bfc6981aef7e2b974fef5c
Tim Schwartz <timatron@gmail.com>
2013-03-14 18:32:31 -0700

    fixing url

--- printQrCode.py
+++ printQrCode.py
@@ -6,7 +6,6 @@ import Image, ImageDraw
 from lib.pyqrcode.pyqrcode import *
 from lib.pythermalprinter.printer import *
 
-
 if len(sys.argv) == 2:
     url = sys.argv[1]
 else:
@@ -14,6 +13,7 @@ else:
 
 qr_image = MakeQRImage(url)
 qr_image.show()
+print "done making code"
 
 serialport = ThermalPrinter.SERIALPORT
 if not os.path.exists(serialport):
@@ -21,8 +21,11 @@ if not os.path.exists(serialport):
 
 p = ThermalPrinter(serialport=serialport)
 
+print "printer initalized"
 data = list(qr_image.getdata())
+print "extracted data from image"
 w, h = qr_image.size
+print "got size info"
 p.print_bitmap(data, w, h, True)
 p.linefeed()
 p.linefeed()
